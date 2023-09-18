318c101ce8c912113c3a13d49a9708460d08d354
esholland85 <esholland85@gmail.com>
2023-09-18 15:57:10 -0700

    reformatted user output, added a template to send to the chatbot, fixed parsing to account for the expected template, opened access from localhost to all interfaces.

--- main.go
+++ main.go
@@ -19,10 +19,10 @@ import (
 func main() {
 	r := chi.NewRouter()
 	char := chi.NewRouter()
-	adminR := chi.NewRouter()
+	//adminR := chi.NewRouter()
 	corsWrapped := middlewareCors(r)
 	httpServer := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    ":8080",
 		Handler: corsWrapped,
 	}
 
@@ -33,10 +33,9 @@ func main() {
 	apiCFG.context_path = "./resources/.context"
 	apiCFG.mux = &sync.RWMutex{}
 
-	r.Mount("/admin", adminR)
+	//r.Mount("/admin", adminR)
 	r.Mount("/character", char)
-	adminR.Get("/", apiCFG.gen_background)
-	char.Get("/", landing_page)
+	char.Get("/", apiCFG.landing_page)
 
 	httpServer.ListenAndServe()
 }
@@ -104,7 +103,7 @@ func (apiCFG *apiConfig) Load_context() (npc_context, error) {
 	return dbData, nil
 }
 
-func construct_message(myContext npc_context, aiQuery string) ([]openai.ChatCompletionMessage, error) {
+func construct_message(myContext npc_context) ([]openai.ChatCompletionMessage, error) {
 	stringSlice := []string{
 		myContext.Technology_level,
 		myContext.Magic_level,
@@ -121,8 +120,38 @@ func construct_message(myContext npc_context, aiQuery string) ([]openai.ChatComp
 			Content: contextString,
 		},
 		{
-			Role:    openai.ChatMessageRoleUser,
-			Content: aiQuery,
+			Role: openai.ChatMessageRoleSystem,
+			Content: "Please provide information in the following format:\n" +
+				"Strength: \n" +
+				"Dexterity: \n" +
+				"Constitution: \n" +
+				"Intelligence: \n" +
+				"Wisdom: \n" +
+				"Charisma: \n" +
+				"Name: \n" +
+				"Gender: \n" +
+				"Age: \n" +
+				"Relationships: ?\n" +
+				"Motivation: \n" +
+				"Background: \n" +
+				"Appearance: ",
+		},
+		{
+			Role: openai.ChatMessageRoleUser,
+			Content: `Please provide information for the following fields:\n
+			What is the character's Strength?\n
+			What is the character's Dexterity?\n
+			What is the character's Constitution?\n
+			What is the character's Intelligence?\n
+			What is the character's Wisdom?\n
+			What is the character's Charisma?\n
+			What is the character's Name?\n
+			What is the character's Gender?\n
+			What is the character's Age?\n
+			Tell me about the character's Relationships.\n
+			What motivates the character?\n
+			Describe the character's Background.\n
+			Please provide details about the character's Appearance.`,
 		},
 	}
 
@@ -168,54 +197,45 @@ func respondWithHTML(w http.ResponseWriter, code int, payload string) {
 	w.Write([]byte(payload))
 }
 
-func (apiCFG *apiConfig) gen_background(w http.ResponseWriter, r *http.Request) {
+func (apiCFG *apiConfig) gen_background(w http.ResponseWriter, r *http.Request) (string, error) {
 	myContext, err := apiCFG.Load_context()
 	if err != nil {
-		respondWithError(w, 500, "Internal Server Error")
-		return
+		return "", nil
 	}
 
-	values := r.URL.Query()
-	if len(values) == 0 && r.Method == "GET" {
-		http.ServeFile(w, r, "./index.html")
-		return
+	if r.URL.Query().Get("fname") != "" {
+		myContext.Character_name = "The character's name is " + r.URL.Query().Get("tech") + "."
 	} else {
-		//Magic_level            string
-		if r.URL.Query().Get("fname") != "" {
-			myContext.Character_name = "Your name is " + r.URL.Query().Get("tech") + "."
-		} else {
-			myContext.Character_name = "Please pick a name appropriate to the technology level."
-		}
-		if r.URL.Query().Get("size") != "Random" && r.URL.Query().Get("size") != "" {
-			myContext.Town_size = "You live in a town of " + r.URL.Query().Get("tech") + " people."
-		} else if r.URL.Query().Get("size") == "Random" {
-			myContext.Town_size = "Randomly choose how big a town you are from."
-		} else {
-			fmt.Println("Incompatible size selected")
-		}
-		if r.URL.Query().Get("tech") != "Random" && r.URL.Query().Get("tech") != "" {
-			myContext.Technology_level = "The current technology level is " + r.URL.Query().Get("tech") + "."
-		} else if r.URL.Query().Get("tech") == "Random" {
-			myContext.Technology_level = "Randomly choose what technological age the world is in."
-		} else {
-			fmt.Println("Incompatible technology query.")
-		}
-		if r.FormValue("mLevel") != "Random" {
-			myContext.Magic_level = "Magic is " + r.FormValue("mLevel") + " in this world."
-		} else {
-			myContext.Magic_level = "On a scale from 0-100 where zero is no magic at all and 100 is magic is used for every-day tasks, randomly select how accessible magic is in this world."
-		}
-		if r.FormValue("role") != "" {
-			myContext.Character_identity = "Your role in society is as follows: " + r.FormValue("role")
-		} else {
-			myContext.Character_identity = "Please choose an appropriate background and role in society given town you're in and the technology level of this world."
-		}
+		myContext.Character_name = "Please pick a name appropriate to the technology level."
+	}
+	if r.URL.Query().Get("size") != "Random" && r.URL.Query().Get("size") != "" {
+		myContext.Town_size = "The character lives in a town of " + r.URL.Query().Get("tech") + " people."
+	} else if r.URL.Query().Get("size") == "Random" {
+		myContext.Town_size = "Randomly choose how big a town the character is from."
+	} else {
+		fmt.Println("Incompatible size selected")
+	}
+	if r.URL.Query().Get("tech") != "Random" && r.URL.Query().Get("tech") != "" {
+		myContext.Technology_level = "The current technology level is " + r.URL.Query().Get("tech") + "."
+	} else if r.URL.Query().Get("tech") == "Random" {
+		myContext.Technology_level = "Randomly choose what technological age the world is in."
+	} else {
+		fmt.Println("Incompatible technology query.")
+	}
+	if r.FormValue("mLevel") != "Random" {
+		myContext.Magic_level = "Magic is " + r.FormValue("mLevel") + " in this world."
+	} else {
+		myContext.Magic_level = "On a scale from 0-100 where zero is no magic at all and 100 is magic is used for every-day tasks, randomly select how accessible magic is in this world."
+	}
+	if r.FormValue("role") != "" {
+		myContext.Character_identity = "The character's role in society is as follows: " + r.FormValue("role")
+	} else {
+		myContext.Character_identity = "Please choose an appropriate background and role in society given the town the characters is in and the technology level of this world."
 	}
 
-	myMessage, err := construct_message(myContext, "Tell me about your role in the world.")
+	myMessage, err := construct_message(myContext)
 	if err != nil {
-		respondWithError(w, 500, "Internal Server Error")
-		return
+		return "", err
 	}
 	client := openai.NewClient(apiCFG.gptKey)
 	resp, err := client.CreateChatCompletion(
@@ -227,27 +247,51 @@ func (apiCFG *apiConfig) gen_background(w http.ResponseWriter, r *http.Request) 
 	)
 
 	if err != nil {
-		respondWithError(w, 500, "Internal Server Error")
-		return
+		return "", err
 	}
 
-	respondWithJSON(w, 200, resp.Choices[0].Message.Content)
+	return resp.Choices[0].Message.Content, nil
 }
 
-func landing_page(w http.ResponseWriter, r *http.Request) {
+func (apiCFG *apiConfig) landing_page(w http.ResponseWriter, r *http.Request) {
 	values := r.URL.Query()
 	if len(values) == 0 && r.Method == "GET" {
 		http.ServeFile(w, r, "./index.html")
 	} else {
+		response, err := apiCFG.gen_background(w, r)
+		if err != nil {
+			respondWithError(w, 500, "Internal Server Error")
+			return
+		}
+
+		split_resp := strings.Split(response, "\n")
+		if len(split_resp) != 13 {
+			respondWithError(w, 500, "Template mismatch, you get raw data!\n\n"+response)
+			return
+		}
+		//take each line from the response, split it at the colon, take the second half, remove the leading space.
 		fields := templateFields{
-			Name: r.URL.Query().Get("fname"),
-			Size: r.URL.Query().Get("size"),
+			Strength:      strings.Split(split_resp[0], ":")[1][1:],
+			Dexterity:     strings.Split(split_resp[1], ":")[1][1:],
+			Constitution:  strings.Split(split_resp[2], ":")[1][1:],
+			Intelligence:  strings.Split(split_resp[3], ":")[1][1:],
+			Wisdom:        strings.Split(split_resp[4], ":")[1][1:],
+			Charisma:      strings.Split(split_resp[5], ":")[1][1:],
+			Name:          strings.Split(split_resp[6], ":")[1][1:],
+			Gender:        strings.Split(split_resp[7], ":")[1][1:],
+			Age:           strings.Split(split_resp[8], ":")[1][1:],
+			Relationships: strings.Split(split_resp[9], ":")[1][1:],
+			Motivation:    strings.Split(split_resp[10], ":")[1][1:],
+			Background:    strings.Split(split_resp[11], ":")[1][1:],
+			Appearance:    strings.Split(split_resp[12], ":")[1][1:],
 		}
 		tmpl, err := loadTemplate()
 		if err != nil {
 			respondWithError(w, 500, "Internal Server Error")
 		}
 		parseTemplate(w, fields, tmpl)
+		//http.ServeFile(w, r, "./resources/character.html")
+		//respondWithError(w, 500, "response handled internally")
 	}
 }
 
@@ -261,13 +305,25 @@ func loadTemplate() (*template.Template, error) {
 }
 
 type templateFields struct {
-	Name string
-	Size string
+	Strength      string
+	Dexterity     string
+	Constitution  string
+	Intelligence  string
+	Wisdom        string
+	Charisma      string
+	Name          string
+	Gender        string
+	Age           string
+	Relationships string
+	Motivation    string
+	Background    string
+	Appearance    string
 }
 
 func parseTemplate(w http.ResponseWriter, data templateFields, tmpl *template.Template) {
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		fmt.Println("template not executed")
+		respondWithError(w, 500, "Internal Server Error")
 	}
 }
